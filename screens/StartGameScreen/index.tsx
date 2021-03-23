e8fe2b553425da08bc103b67abf78b706d74baa9
donpapitoskey <alarcon.juan@correounivalle.edu.co>
2021-03-23 18:50:34 -0500

    input validation and bluring when pressed outside

--- screens/StartGameScreen/index.tsx
+++ screens/StartGameScreen/index.tsx
@@ -1,29 +1,45 @@
-import React from 'react';
-import {View, Text, Button} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import styles from './styles';
 import {Card, Input} from 'components';
 import {theme} from 'constants/theme';
 
 const StartGameScreen: React.FC = () => {
+  const [enteredValue, setEnteredValue] = useState('');
+
+  const handleInput = (inputText: string) => {
+    setEnteredValue(inputText.replace(/[^0-9]/g, ''));
+  };
+
   return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>Start a new game!</Text>
-      <Card style={styles.inputContainer}>
-        <Text>Select a number</Text>
-        <Input
-          blurOnSubmit
-          autoCapitalize="none"
-          autoCorrect={false}
-          keyboardType="numeric"
-          maxLength={2}
-          style={styles.input}
-        />
-        <View style={styles.buttonContainer}>
-          <Button title="Reset" onPress={() => {}} color={theme.accent} />
-          <Button title="Confirm" onPress={() => {}} color={theme.primary} />
-        </View>
-      </Card>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss.bind(null)}>
+      <View style={styles.screen}>
+        <Text style={styles.title}>Start a new game!</Text>
+        <Card style={styles.inputContainer}>
+          <Text>Select a number</Text>
+          <Input
+            blurOnSubmit
+            autoCapitalize="none"
+            autoCorrect={false}
+            keyboardType="number-pad"
+            maxLength={2}
+            style={styles.input}
+            onChangeText={handleInput}
+            value={enteredValue}
+          />
+          <View style={styles.buttonContainer}>
+            <Button title="Reset" onPress={() => {}} color={theme.accent} />
+            <Button title="Confirm" onPress={() => {}} color={theme.primary} />
+          </View>
+        </Card>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
