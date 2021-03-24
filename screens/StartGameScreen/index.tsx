d80d9585e3c944931d98ef8cffb41266ee5b5837
donpapitoskey <alarcon.juan@correounivalle.edu.co>
2021-03-23 19:16:42 -0500

    functionality of confirm button

--- screens/StartGameScreen/index.tsx
+++ screens/StartGameScreen/index.tsx
@@ -12,10 +12,31 @@ import {theme} from 'constants/theme';
 
 const StartGameScreen: React.FC = () => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [confirmed, setConfirmed] = useState(false);
+  const [selectdNumber, setSelectedNumber] = useState(0);
 
   const handleInput = (inputText: string) => {
     setEnteredValue(inputText.replace(/[^0-9]/g, ''));
   };
+
+  const resetInputHandler = () => {
+    setEnteredValue('');
+    setConfirmed(false);
+  };
+
+  const confirmInputHandler = () => {
+    const chosenNumber = parseInt(enteredValue, 10);
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      return;
+    }
+    setConfirmed(true);
+    setEnteredValue('');
+    setSelectedNumber(chosenNumber);
+  };
+  let confirmedOutput;
+  if (confirmed) {
+    confirmedOutput = <Text>Chosen Number: {selectdNumber}</Text>;
+  }
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss.bind(null)}>
@@ -34,10 +55,19 @@ const StartGameScreen: React.FC = () => {
             value={enteredValue}
           />
           <View style={styles.buttonContainer}>
-            <Button title="Reset" onPress={() => {}} color={theme.accent} />
-            <Button title="Confirm" onPress={() => {}} color={theme.primary} />
+            <Button
+              title="Reset"
+              onPress={resetInputHandler}
+              color={theme.accent}
+            />
+            <Button
+              title="Confirm"
+              onPress={confirmInputHandler}
+              color={theme.primary}
+            />
           </View>
         </Card>
+        {confirmedOutput}
       </View>
     </TouchableWithoutFeedback>
   );
